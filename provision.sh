10cde5d83b6e925aca95d8a7af18edbc980ec45e
vit0r <vitornascimentoaraujo@gmail.com>
2021-03-05 13:00:53 -0300

    remove fp

--- provision.sh
+++ provision.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 # -*- coding: utf-8 -*-
 
-readonly FLASH_PLAYER_INSTALL_URL="https://raw.githubusercontent.com/cybernova/fireflashupdate/master/fireflashupdate.sh"
+# readonly FLASH_PLAYER_INSTALL_URL="https://raw.githubusercontent.com/cybernova/fireflashupdate/master/fireflashupdate.sh"
 
 init 3
 runlevel  
@@ -18,16 +18,16 @@ systemctl enable tor
 
 usermod -a -G audio vagrant
 
-wget ${FLASH_PLAYER_INSTALL_URL}
-chmod +x /home/vagrant/fireflashupdate.sh
-sh /home/vagrant/fireflashupdate.sh
-rm /home/vagrant/fireflashupdate.sh
+# wget ${FLASH_PLAYER_INSTALL_URL}
+# chmod +x /home/vagrant/fireflashupdate.sh
+# sh /home/vagrant/fireflashupdate.sh
+# rm /home/vagrant/fireflashupdate.sh
 
 cp -arv /vagrant/etc/proxychains.conf /etc/proxychains.conf
 cp -arv /vagrant/etc/interfaces /etc/network/interfaces
 
 ln -s /usr/bin/ls /usr/bin/ll
 
-#apt upgrade -y
+apt upgrade -y
 
 reboot
